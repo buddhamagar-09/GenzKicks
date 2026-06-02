67109b77bd2aef1c0adbe3981ef04e33acc0c972
Buddha Saru Magar <busa_bca2080@lict.edu.np>
2026-06-02 11:24:45 +0545

    separate files for the dashboard Components

---
+++ resources/views/admin/home/sidebar.blade.php
@@ -0,0 +1,36 @@
+     <div class="d-flex align-items-stretch">
+    <!-- Sidebar Navigation-->
+    <nav id="sidebar">
+      <!-- Sidebar Header-->
+      <div class="sidebar-header d-flex align-items-center">
+        <div class="avatar"><img src="img/avatar-6.jpg" alt="..." class="img-fluid rounded-circle"></div>
+        <div class="title">
+          <h1 class="h5">Buddha Saru Magar</h1>
+          <p>Web Designer</p>
+        </div>
+      </div>
+      <!-- Sidebar Navidation Menus--><span class="heading">Main</span>
+      <ul class="list-unstyled">
+        <li class="active"><a href="index.html"> <i class="icon-home"></i>Home </a></li>
+        <li><a href="#exampledropdownDropdown" aria-expanded="false" data-toggle="collapse"> <i
+              class="icon-windows"></i>Example dropdown </a>
+          <ul id="exampledropdownDropdown" class="collapse list-unstyled ">
+            <li><a href="#">Page</a></li>
+            <li><a href="#">Page</a></li>
+            <li><a href="#">Page</a></li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
+    <!-- Sidebar Navigation end-->
+    <div class="page-content">
+      <div class="page-header">
+        <div class="container-fluid">
+          <h2 class="h5 no-margin-bottom">Dashboard</h2>
+        </div>
+      </div>
+    
+
+    
+      
+   
