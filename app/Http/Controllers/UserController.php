139ca7e407fc2c546abeaa51cfa938abcf4610a0
Buddha Saru Magar <busa_bca2080@lict.edu.np>
2026-07-07 19:36:07 +0545

    function create for shop contact why testimonial product details page

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class UserController extends Controller
 {
@@ -13,5 +14,27 @@ class UserController extends Controller
         } else if (Auth::check() && Auth::user()->usertype == 'admin') {
             return view('admin.dashboard');
         }
+    }
+
+    function contact()
+    {
+        return view('contact');
+    }
+    function shop()
+    {
+        return view('shop');
+    }
+    function testimonial()
+    {
+        return view('testimonial');
+    }
+    function why()
+    {
+        return view('why');
+    }
+    function product_detail(string $id)
+    {
+        $product = Product::find($id);
+        return view('productdetails', ['pdetail' => $product]);
     }
 }
